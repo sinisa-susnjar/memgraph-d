461d4a7f9f4ddbf13ced9f1bb10ec9c8b7f1c60b
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-07-08 23:36:06 +0100

    added Value struct

--- source/memgraph.d
+++ source/memgraph.d
@@ -4,15 +4,259 @@ public import mgclient;
 
 import std.string;
 
+struct Detail {
+	static Value.Type ConvertType(mg_value_type type) {
+		import std.exception, std.conv;
+		switch (type) {
+			case mg_value_type.MG_VALUE_TYPE_NULL:
+				return Value.Type.Null;
+			case mg_value_type.MG_VALUE_TYPE_BOOL:
+				return Value.Type.Bool;
+			case mg_value_type.MG_VALUE_TYPE_INTEGER:
+				return Value.Type.Int;
+			case mg_value_type.MG_VALUE_TYPE_FLOAT:
+				return Value.Type.Double;
+			case mg_value_type.MG_VALUE_TYPE_STRING:
+				return Value.Type.String;
+			case mg_value_type.MG_VALUE_TYPE_LIST:
+				return Value.Type.List;
+			case mg_value_type.MG_VALUE_TYPE_MAP:
+				return Value.Type.Map;
+			case mg_value_type.MG_VALUE_TYPE_NODE:
+				return Value.Type.Node;
+			case mg_value_type.MG_VALUE_TYPE_RELATIONSHIP:
+				return Value.Type.Relationship;
+			case mg_value_type.MG_VALUE_TYPE_UNBOUND_RELATIONSHIP:
+				return Value.Type.UnboundRelationship;
+			case mg_value_type.MG_VALUE_TYPE_PATH:
+				return Value.Type.Path;
+			case mg_value_type.MG_VALUE_TYPE_DATE:
+				return Value.Type.Date;
+			case mg_value_type.MG_VALUE_TYPE_TIME:
+				return Value.Type.Time;
+			case mg_value_type.MG_VALUE_TYPE_LOCAL_TIME:
+				return Value.Type.LocalTime;
+			case mg_value_type.MG_VALUE_TYPE_DATE_TIME:
+				return Value.Type.DateTime;
+			case mg_value_type.MG_VALUE_TYPE_DATE_TIME_ZONE_ID:
+				return Value.Type.DateTimeZoneId;
+			case mg_value_type.MG_VALUE_TYPE_LOCAL_DATE_TIME:
+				return Value.Type.LocalDateTime;
+			case mg_value_type.MG_VALUE_TYPE_DURATION:
+				return Value.Type.Duration;
+			case mg_value_type.MG_VALUE_TYPE_POINT_2D:
+				return Value.Type.Point2d;
+			case mg_value_type.MG_VALUE_TYPE_POINT_3D:
+				return Value.Type.Point3d;
+			case mg_value_type.MG_VALUE_TYPE_UNKNOWN:
+				throw new Exception("Unknown value type!");
+			default:
+				assert(0, "unexpected type: " ~ to!string(type));
+		}
+	}
+}
+
 struct ConstMap {
 	mg_map *_map;
 }
 
 struct Value {
-	this(const mg_value *value) {
-		_value = value;
+	/// \brief Types that can be stored in a `Value`.
+	enum Type {
+		Null,
+		Bool,
+		Int,
+		Double,
+		String,
+		List,
+		Map,
+		Node,
+		Relationship,
+		UnboundRelationship,
+		Path,
+		Date,
+		Time,
+		LocalTime,
+		DateTime,
+		DateTimeZoneId,
+		LocalDateTime,
+		Duration,
+		Point2d,
+		Point3d
 	}
-	const mg_value *_value;
+
+	/// \brief Constructs an object that becomes the owner of the given `value`.
+	/// `value` is destroyed when a `Value` object is destroyed.
+	this(mg_value *ptr) { ptr_ = ptr; }
+
+	/// \brief Creates a Value from a copy of the given \ref mg_value.
+	this(const mg_value *const_ptr) { this(mg_value_copy(const_ptr)); }
+
+	this(const ref Value other) { this(mg_value_copy(other.ptr_)); }
+	// Value(Value &&other);
+	// Value &operator=(const Value &other) = delete;
+	// Value &operator=(Value &&other) = delete;
+	~this() {
+		if (ptr_ != null)
+			mg_value_destroy(ptr_);
+	}
+
+	// explicit Value(const ConstValue &value);
+
+	/// \brief Creates Null value.
+	// this() { this(mg_value_make_null()); }
+
+	// Constructors for primitive types:
+	this(bool value) { this(mg_value_make_bool(value)); }
+	this(int value) { this(mg_value_make_integer(value)); }
+	this(long value) { this(mg_value_make_integer(value)); }
+	this(double value) { this(mg_value_make_float(value)); }
+
+	// Constructors for string:
+	this(const ref string value) {
+		this(mg_value_make_string(toStringz(value)));
+	}
+	// explicit Value(const char *value);
+
+
+	/// \brief Constructs a list value and takes the ownership of the `list`.
+	/// \note
+	/// Behaviour of accessing the `list` after performing this operation is
+	/// considered undefined.
+	// this(List &&list);
+
+	/// \brief Constructs a map value and takes the ownership of the `map`.
+	/// \note
+	/// Behaviour of accessing the `map` after performing this operation is
+	/// considered undefined.
+	// this(Map &&map);
+
+	/// \brief Constructs a vertex value and takes the ownership of the given
+	/// `vertex`. \note Behaviour of accessing the `vertex` after performing this
+	/// operation is considered undefined.
+	// explicit Value(Node &&vertex);
+
+	/// \brief Constructs an edge value and takes the ownership of the given
+	/// `edge`. \note Behaviour of accessing the `edge` after performing this
+	/// operation is considered undefined.
+	// explicit Value(Relationship &&edge);
+
+	/// \brief Constructs an unbounded edge value and takes the ownership of the
+	/// given `edge`. \note Behaviour of accessing the `edge` after performing
+	/// this operation is considered undefined.
+	// explicit Value(UnboundRelationship &&edge);
+
+	/// \brief Constructs a path value and takes the ownership of the given
+	/// `path`. \note Behaviour of accessing the `path` after performing this
+	/// operation is considered undefined.
+	// explicit Value(Path &&path);
+
+
+	/// \brief Constructs a date value and takes the ownership of the given
+	/// `date`. \note Behaviour of accessing the `date` after performing this
+	/// operation is considered undefined.
+	// explicit Value(Date &&date);
+
+	/// \brief Constructs a time value and takes the ownership of the given
+	/// `time`. \note Behaviour of accessing the `time` after performing this
+	/// operation is considered undefined.
+	// explicit Value(Time &&time);
+
+	/// \brief Constructs a LocalTime value and takes the ownership of the given
+	/// `localTime`. \note Behaviour of accessing the `localTime` after performing
+	/// this operation is considered undefined.
+	// explicit Value(LocalTime &&localTime);
+
+	/// \brief Constructs a DateTime value and takes the ownership of the given
+	/// `dateTime`. \note Behaviour of accessing the `dateTime` after performing
+	/// this operation is considered undefined.
+	// explicit Value(DateTime &&dateTime);
+
+	/// \brief Constructs a DateTimeZoneId value and takes the ownership of the
+	/// given `dateTimeZoneId`. \note Behaviour of accessing the `dateTimeZoneId`
+	/// after performing this operation is considered undefined.
+	// explicit Value(DateTimeZoneId &&dateTimeZoneId);
+
+	/// \brief Constructs a LocalDateTime value and takes the ownership of the
+	/// given `localDateTime`. \note Behaviour of accessing the `localDateTime`
+	/// after performing this operation is considered undefined.
+	// explicit Value(LocalDateTime &&localDateTime);
+
+	/// \brief Constructs a Duration value and takes the ownership of the given
+	/// `duration`. \note Behaviour of accessing the `duration` after performing
+	/// this operation is considered undefined.
+	// explicit Value(Duration &&duration);
+
+	/// \brief Constructs a Point2d value and takes the ownership of the given
+	/// `point2d`. \note Behaviour of accessing the `point2d` after performing
+	/// this operation is considered undefined.
+	// explicit Value(Point2d &&point2d);
+
+	/// \brief Constructs a Point3d value and takes the ownership of the given
+	/// `point3d`. \note Behaviour of accessing the `point3d` after performing
+	/// this operation is considered undefined.
+	// explicit Value(Point3d &&point3d);
+
+	/// \pre value type is Type::Bool
+	// bool ValueBool() const;
+	/// \pre value type is Type::Int
+	// int64_t ValueInt() const;
+	/// \pre value type is Type::Double
+	// double ValueDouble() const;
+	/// \pre value type is Type::String
+	// std::string_view ValueString() const;
+	/// \pre value type is Type::List
+	// const ConstList ValueList() const;
+	/// \pre value type is Type::Map
+	// const ConstMap ValueMap() const;
+	/// \pre value type is Type::Node
+	// const ConstNode ValueNode() const;
+	/// \pre value type is Type::Relationship
+	// const ConstRelationship ValueRelationship() const;
+	/// \pre value type is Type::UnboundRelationship
+	// const ConstUnboundRelationship ValueUnboundRelationship() const;
+	/// \pre value type is Type::Path
+	// const ConstPath ValuePath() const;
+	/// \pre value type is Type::Date
+	// const ConstDate ValueDate() const;
+	/// \pre value type is Type::Time
+	// const ConstTime ValueTime() const;
+	/// \pre value type is Type::LocalTime
+	// const ConstLocalTime ValueLocalTime() const;
+	/// \pre value type is Type::DateTime
+	// const ConstDateTime ValueDateTime() const;
+	/// \pre value type is Type::DateTimeZoneId
+	// const ConstDateTimeZoneId ValueDateTimeZoneId() const;
+	/// \pre value type is Type::LocalDateTime
+	// const ConstLocalDateTime ValueLocalDateTime() const;
+
+	/// \pre value type is Type::Duration
+	//const ConstDuration ValueDuration() const;
+	/// \pre value type is Type::Point2d
+	//const ConstPoint2d ValuePoint2d() const;
+	/// \pre value type is Type::Point3d
+	//const ConstPoint3d ValuePoint3d() const;
+
+	/// \exception std::runtime_error the value type is unknown
+	Type type() const {
+		return Detail.ConvertType(mg_value_get_type(ptr_));
+	}
+
+	//ConstValue AsConstValue() const;
+
+	/// \exception std::runtime_error the value type is unknown
+	//bool operator==(const Value &other) const;
+	/// \exception std::runtime_error the value type is unknown
+	//bool operator==(const ConstValue &other) const;
+	/// \exception std::runtime_error the value type is unknown
+	//bool operator!=(const Value &other) const { return !(*this == other); }
+	/// \exception std::runtime_error the value type is unknown
+	//bool operator!=(const ConstValue &other) const { return !(*this == other); }
+
+	//const mg_value *ptr() const { return ptr_; }
+
+private:
+	mg_value *ptr_;
 }
 
 struct Optional(V) {
