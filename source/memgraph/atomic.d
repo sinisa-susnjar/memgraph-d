aaeb2c69e9454ff59ee468b052ad74996ae05c1c
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-07-28 22:51:57 +0100

    added todo, new unit test

--- source/memgraph/atomic.d
+++ source/memgraph/atomic.d
@@ -8,7 +8,7 @@ struct AtomicRef(T, alias Dtor)
 {
 	@disable this();
 
-	/// Create a new instance using a pointer to T `ptr` and an initial reference `count`.
+	/// Create a new instance using a pointer to T `ptr` and an optional initial reference `count`.
 	this(T *ptr, uint count = 1) {
 		ptr_ = ptr;
 		atomicStore(refs_, count);
@@ -32,6 +32,10 @@ struct AtomicRef(T, alias Dtor)
 
 	auto ptr() const { return ptr_; }
 
+	// TODO: disable copying for now - need to invest some time to make it a copyable
+	//       AtomicRef - will probably need to have a control structure like shared_ptr
+	@disable void opAssign(AtomicRef!(T, Dtor) rhs); // @safe return { }
+
 private:
 	T *ptr_ = null;
 	shared uint refs_ = 1;
@@ -48,9 +52,24 @@ unittest {
 		p.greeting = "Live long and prosper";
 		writefln("p: %s", p.greeting);
 
+		// AtomicRef can be used with GC allocated memory
 		auto a1 = new AtomicRef!(Dummy, free)(p);
 	}
 	// Force garbage collection for full code coverage
 	import core.memory;
 	GC.collect();
+}
+
+unittest {
+	struct Dummy {
+		string greeting;
+	}
+	import core.stdc.stdlib : malloc, free;
+	auto p = cast(Dummy *)malloc(Dummy.sizeof);
+	import std.stdio;
+	p.greeting = "Live long and prosper";
+	writefln("p: %s", p.greeting);
+
+	// AtomicRef can be used with scope memory
+	auto a1 = AtomicRef!(Dummy, free)(p);
 }
