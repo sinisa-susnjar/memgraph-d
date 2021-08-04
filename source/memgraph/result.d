3a2b19bc9de046783b71ae3defffd7906d9788db
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-08-04 21:13:31 +0100

    replaced hamfisted ref count with proper shared pointer

--- source/memgraph/result.d
+++ source/memgraph/result.d
@@ -4,6 +4,7 @@ module memgraph.result;
 import std.string, std.conv;
 
 import memgraph.mgclient, memgraph.value, memgraph.map, memgraph.optional, memgraph.detail;
+import memgraph.atomic;
 
 /// An object encapsulating a single result row or query execution summary. It's
 /// lifetime is limited by lifetime of parent `mg_session`. Also, invoking
@@ -12,9 +13,9 @@ import memgraph.mgclient, memgraph.value, memgraph.map, memgraph.optional, memgr
 struct Result {
 	/// Returns names of columns output by the current query execution.
 	auto columns() {
-		assert(result != null);
-		assert(*result != null);
-		const (mg_list) *list = mg_result_columns(*result);
+		assert(ref_.data != null);
+		assert(*ref_.data != null);
+		const (mg_list) *list = mg_result_columns(*ref_.data);
 		const size_t list_length = mg_list_size(list);
 		string[] cols;
 		cols.length = list_length;
@@ -25,9 +26,9 @@ struct Result {
 
 	/// Returns query execution summary as a key/value `Map`.
 	auto summary() {
-		assert(result != null);
-		assert(*result != null);
-		return Map(mg_result_summary(*result));
+		assert(ref_.data != null);
+		assert(*ref_.data != null);
+		return Map(mg_result_summary(*ref_.data));
 	}
 
 	/// Check if the `Result` is empty.
@@ -35,11 +36,11 @@ struct Result {
 	/// Note: part of `InputRange` interface
 	bool empty() {
 		if (values.length == 0) {
-			assert(result != null);
-			immutable status = mg_session_fetch(session, result);
+			assert(ref_.data != null);
+			immutable status = mg_session_fetch(session, ref_.data);
 			if (status != 1)
 				return true;
-			const (mg_list) *list = mg_result_row(*result);
+			const (mg_list) *list = mg_result_row(*ref_.data);
 			const size_t list_length = mg_list_size(list);
 			Value[] data;
 			data.length = list_length;
@@ -61,25 +62,6 @@ struct Result {
 		values = values[1..$];
 	}
 
-	/// Postblit constructor. Increases `mg_result` reference count for each copy made.
-	this(this) {
-		// Increase reference count for result pointer.
-		n++;
-	}
-
-	/// Destructor, counts down `mg_result` reference count and frees memory if count goes to zero.
-	~this() {
-		// One `Result` instance gone, reduce reference count.
-		if (!--n) {
-			// Ref count is 0 - this means the original `Result` instance is being destructed,
-			// the `mg_result` pointer is not needed anymore, free it.
-			import core.stdc.stdlib : free;
-			if (result != null)
-				free(result);
-			result = null;
-		}
-	}
-
 package:
 	/// Initial construction of a `Result` from the given `mg_session` pointer.
 	/// Ranges in D first perform a copy of the range object on which they will
@@ -88,19 +70,16 @@ package:
 	/// Allocate a reference counted `mg_result` pointer to be shared with all
 	/// future range copies.
 	this(mg_session *session) {
-		import core.stdc.stdlib : malloc;
+		import core.stdc.stdlib : malloc, free;
 		this.session = session;
-		result = cast(mg_result **)malloc((mg_result *).sizeof);
-		assert(result != null);
+		ref_ = SharedPtr!(mg_result*).make(cast(mg_result **)malloc((mg_result *).sizeof), (p) { free(p); });
 	}
 
 private:
 	/// Pointer to private `mg_session` instance.
 	mg_session *session;
-	/// Pointer to private `mg_result` instance.
-	mg_result **result;
-	/// Reference count for `mg_result` pointer. Needed for ranges.
-	size_t n = 1;
+	/// Shared pointer to private `mg_result` instance.
+	SharedPtr!(mg_result*) ref_;
 	/// Temporary value store.
 	Value[][] values;
 }
