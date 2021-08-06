52f188c3c9c50103eff22f110f89b443c6a42cdf
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-08-06 07:12:57 +0100

    added asserts

--- source/memgraph/detail.d
+++ source/memgraph/detail.d
@@ -8,6 +8,7 @@ import memgraph.mgclient, memgraph.enums;
 /// Wrapper class around static helper functions.
 struct Detail {
 	static string convertString(const mg_string *str) {
+		assert(str != null);
 		const auto data = mg_string_data(str);
 		const auto len = mg_string_size(str);
 		return to!string(data[0..len]);
@@ -63,6 +64,8 @@ struct Detail {
 	}
 
 	static bool areValuesEqual(const mg_value *value1, const mg_value *value2) {
+		assert(value1 != null);
+		assert(value2 != null);
 		if (value1 == value2) {
 			return true;
 		}
@@ -134,40 +137,37 @@ struct Detail {
 	}
 
 	static bool areListsEqual(const mg_list *list1, const mg_list *list2) {
-		if (list1 == list2) {
+		assert(list1 != null);
+		assert(list2 != null);
+		if (list1 == list2)
 			return true;
-		}
-		if (mg_list_size(list1) != mg_list_size(list2)) {
+		if (mg_list_size(list1) != mg_list_size(list2))
 			return false;
-		}
 		const uint len = mg_list_size(list1);
 		for (uint i = 0; i < len; ++i) {
-			if (!Detail.areValuesEqual(mg_list_at(list1, i), mg_list_at(list2, i))) {
+			if (!Detail.areValuesEqual(mg_list_at(list1, i), mg_list_at(list2, i)))
 				return false;
-			}
 		}
 		return true;
 	}
 
 	static bool areMapsEqual(const mg_map *map1, const mg_map *map2) {
-		if (map1 == map2) {
+		assert(map1 != null);
+		assert(map2 != null);
+		if (map1 == map2)
 			return true;
-		}
-		if (mg_map_size(map1) != mg_map_size(map2)) {
+		if (mg_map_size(map1) != mg_map_size(map2))
 			return false;
-		}
 		const uint len = mg_map_size(map1);
 		for (uint i = 0; i < len; ++i) {
 			const mg_string *key = mg_map_key_at(map1, i);
 			const mg_value *value1 = mg_map_value_at(map1, i);
 			const mg_value *value2 =
 				mg_map_at2(map2, mg_string_size(key), mg_string_data(key));
-			if (value2 == null) {
+			if (value2 == null)
 				return false;
-			}
-			if (!Detail.areValuesEqual(value1, value2)) {
+			if (!Detail.areValuesEqual(value1, value2))
 				return false;
-			}
 		}
 		return true;
 	}
@@ -177,15 +177,14 @@ struct Detail {
 	///         node2 = second node to compare
 	/// Return: `true` if both nodes are equal, `false` otherwise
 	static bool areNodesEqual(const mg_node *node1, const mg_node *node2) {
-		if (node1 == node2) {
+		assert(node1 != null);
+		assert(node2 != null);
+		if (node1 == node2)
 			return true;
-		}
-		if (mg_node_id(node1) != mg_node_id(node2)) {
+		if (mg_node_id(node1) != mg_node_id(node2))
 			return false;
-		}
-		if (mg_node_label_count(node1) != mg_node_label_count(node2)) {
+		if (mg_node_label_count(node1) != mg_node_label_count(node2))
 			return false;
-		}
 		string[] labels1;
 		string[] labels2;
 		const uint label_count = mg_node_label_count(node1);
@@ -194,58 +193,53 @@ struct Detail {
 			labels1[i] = Detail.convertString(mg_node_label_at(node1, i));
 			labels2[i] = Detail.convertString(mg_node_label_at(node2, i));
 		}
-		if (labels1 != labels2) {
+		if (labels1 != labels2)
 			return false;
-		}
 		return Detail.areMapsEqual(mg_node_properties(node1),
 				mg_node_properties(node2));
 	}	// areNodesEqual()
 
 	static bool areRelationshipsEqual(const mg_relationship *rel1,
 			const mg_relationship *rel2) {
-		if (rel1 == rel2) {
+		assert(rel1 != null);
+		assert(rel2 != null);
+		if (rel1 == rel2)
 			return true;
-		}
-		if (mg_relationship_id(rel1) != mg_relationship_id(rel2)) {
+		if (mg_relationship_id(rel1) != mg_relationship_id(rel2))
 			return false;
-		}
-		if (mg_relationship_start_id(rel1) != mg_relationship_start_id(rel2)) {
+		if (mg_relationship_start_id(rel1) != mg_relationship_start_id(rel2))
 			return false;
-		}
-		if (mg_relationship_end_id(rel1) != mg_relationship_end_id(rel2)) {
+		if (mg_relationship_end_id(rel1) != mg_relationship_end_id(rel2))
 			return false;
-		}
 		if (Detail.convertString(mg_relationship_type(rel1)) !=
-				Detail.convertString(mg_relationship_type(rel2))) {
+				Detail.convertString(mg_relationship_type(rel2)))
 			return false;
-		}
 		return Detail.areMapsEqual(mg_relationship_properties(rel1),
 				mg_relationship_properties(rel2));
 	}
 
 	static bool areUnboundRelationshipsEqual(const mg_unbound_relationship *rel1,
 			const mg_unbound_relationship *rel2) {
-		if (rel1 == rel2) {
+		assert(rel1 != null);
+		assert(rel2 != null);
+		if (rel1 == rel2)
 			return true;
-		}
-		if (mg_unbound_relationship_id(rel1) != mg_unbound_relationship_id(rel2)) {
+		if (mg_unbound_relationship_id(rel1) != mg_unbound_relationship_id(rel2))
 			return false;
-		}
 		if (Detail.convertString(mg_unbound_relationship_type(rel1)) !=
-				Detail.convertString(mg_unbound_relationship_type(rel2))) {
+				Detail.convertString(mg_unbound_relationship_type(rel2)))
 			return false;
-		}
 		return Detail.areMapsEqual(mg_unbound_relationship_properties(rel1),
 				mg_unbound_relationship_properties(rel2));
 	}
 
 	static bool arePathsEqual(const mg_path *path1, const mg_path *path2) {
-		if (path1 == path2) {
+		assert(path1 != null);
+		assert(path2 != null);
+		if (path1 == path2)
 			return true;
-		}
-		if (mg_path_length(path1) != mg_path_length(path2)) {
+		if (mg_path_length(path1) != mg_path_length(path2))
 			return false;
-		}
 		const uint len = mg_path_length(path1);
 		for (uint i = 0; i < len; ++i) {
 			if (!Detail.areNodesEqual(mg_path_node_at(path1, i),
@@ -267,22 +261,30 @@ struct Detail {
 	}
 
 	static bool areDatesEqual(const mg_date *date1, const mg_date *date2) {
+		assert(date1 != null);
+		assert(date2 != null);
 		return mg_date_days(date1) == mg_date_days(date2);
 	}
 
 	static bool areTimesEqual(const mg_time *time1, const mg_time *time2) {
+		assert(time1 != null);
+		assert(time2 != null);
 		return mg_time_nanoseconds(time1) == mg_time_nanoseconds(time2) &&
 			mg_time_tz_offset_seconds(time1) == mg_time_tz_offset_seconds(time2);
 	}
 
 	static bool areLocalTimesEqual(const mg_local_time *local_time1,
 			const mg_local_time *local_time2) {
+		assert(local_time1 != null);
+		assert(local_time2 != null);
 		return mg_local_time_nanoseconds(local_time1) ==
 			mg_local_time_nanoseconds(local_time2);
 	}
 
 	static bool areDateTimesEqual(const mg_date_time *date_time1,
 			const mg_date_time *date_time2) {
+		assert(date_time1 != null);
+		assert(date_time2 != null);
 		return mg_date_time_seconds(date_time1) == mg_date_time_seconds(date_time2) &&
 			mg_date_time_nanoseconds(date_time1) ==
 			mg_date_time_nanoseconds(date_time2) &&
@@ -293,6 +295,8 @@ struct Detail {
 	static bool areDateTimeZoneIdsEqual(
 			const mg_date_time_zone_id *date_time_zone_id1,
 			const mg_date_time_zone_id *date_time_zone_id2) {
+		assert(date_time_zone_id1 != null);
+		assert(date_time_zone_id2 != null);
 		return mg_date_time_zone_id_seconds(date_time_zone_id1) ==
 			mg_date_time_zone_id_seconds(date_time_zone_id2) &&
 			mg_date_time_zone_id_nanoseconds(date_time_zone_id1) ==
@@ -303,6 +307,8 @@ struct Detail {
 
 	static bool areLocalDateTimesEqual(const mg_local_date_time *local_date_time1,
 			const mg_local_date_time *local_date_time2) {
+		assert(local_date_time1 != null);
+		assert(local_date_time2 != null);
 		return mg_local_date_time_seconds(local_date_time1) ==
 			mg_local_date_time_seconds(local_date_time2) &&
 			mg_local_date_time_nanoseconds(local_date_time1) ==
@@ -311,6 +317,8 @@ struct Detail {
 
 	static bool areDurationsEqual(const mg_duration *duration1,
 			const mg_duration *duration2) {
+		assert(duration1 != null);
+		assert(duration2 != null);
 		return mg_duration_months(duration1) == mg_duration_months(duration2) &&
 			mg_duration_days(duration1) == mg_duration_days(duration2) &&
 			mg_duration_seconds(duration1) == mg_duration_seconds(duration2) &&
@@ -320,6 +328,8 @@ struct Detail {
 
 	static bool arePoint2dsEqual(const mg_point_2d *point_2d1,
 			const mg_point_2d *point_2d2) {
+		assert(point_2d1 != null);
+		assert(point_2d2 != null);
 		return mg_point_2d_srid(point_2d1) == mg_point_2d_srid(point_2d2) &&
 			mg_point_2d_x(point_2d1) == mg_point_2d_x(point_2d2) &&
 			mg_point_2d_y(point_2d1) == mg_point_2d_y(point_2d2);
@@ -327,6 +337,8 @@ struct Detail {
 
 	static bool arePoint3dsEqual(const mg_point_3d *point_3d1,
 			const mg_point_3d *point_3d2) {
+		assert(point_3d1 != null);
+		assert(point_3d2 != null);
 		return mg_point_3d_srid(point_3d1) == mg_point_3d_srid(point_3d2) &&
 			mg_point_3d_x(point_3d1) == mg_point_3d_x(point_3d2) &&
 			mg_point_3d_y(point_3d1) == mg_point_3d_y(point_3d2) &&
