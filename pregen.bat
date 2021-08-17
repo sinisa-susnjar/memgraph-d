b4b217759ddc82624a58a91ccee58cdff0715c63
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-08-17 20:43:08 +0100

    created a pre-generate commands script for linux and windows

---
+++ pregen.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+IF "%PACKAGE_DIR%"=="" set PACKAGE_DIR=%~dp0
+git submodule update --init --recursive
+mkdir -p %PACKAGE_DIR%\mgclient\build
+cd %PACKAGE_DIR%\mgclient\build
+cmake .. -G "MinGW Makefiles"
+cmake --build .
