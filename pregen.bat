f2b23366b4cba1909dc26db4a779cf10a8ff373f
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-08-21 03:53:03 +0100

    adaptations to windows and msvc

--- pregen.bat
+++ pregen.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 IF "%PACKAGE_DIR%"=="" set PACKAGE_DIR=%~dp0
-git submodule update --init --recursive
+@REM git submodule update --init --recursive
 mkdir %PACKAGE_DIR%\mgclient\build
 cd %PACKAGE_DIR%\mgclient\build
-cmake .. -G "MinGW Makefiles"
+cmake ..
 cmake --build .
