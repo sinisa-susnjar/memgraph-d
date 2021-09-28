ef3a796b7225f5f4ca23d2eb226ad44c5a2f633a
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-09-28 23:05:49 +0100

    let pregen.bat update mgclient submodule

--- pregen.bat
+++ pregen.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%PACKAGE_DIR%"=="" set PACKAGE_DIR=%~dp0
-@REM git submodule update --init --recursive
+git submodule update --init --recursive
 mkdir %PACKAGE_DIR%\mgclient\build
 cd %PACKAGE_DIR%\mgclient\build
 cmake ..
